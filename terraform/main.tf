05bf656d51f839d104c2a41b58e9ddb1b2e6cb67
Mike-Dominate <dele.olagoroye@gmail.com>
2026-09-07 12:31:40 +1000

    Support delegated service subnets

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,6 +28,18 @@ resource "azurerm_subnet" "this" {
   resource_group_name  = azurerm_resource_group.network.name
   virtual_network_name = azurerm_virtual_network.this[each.value.vnet_key].name
   address_prefixes     = each.value.address_prefixes
+
+  dynamic "delegation" {
+    for_each = try(each.value.delegation_name, null) == null ? [] : [each.value.delegation_name]
+
+    content {
+      name = "delegation-${each.key}"
+
+      service_delegation {
+        name = delegation.value
+      }
+    }
+  }
 }
 
 # Stage 02: Hub-and-spoke peering
