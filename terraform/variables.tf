f2c96a35b6dacab9185f98f09e9cef53234e7dd0
Mike-Dominate <dele.olagoroye@gmail.com>
2026-09-07 11:45:00 +1000

    Add Stage 03 routing variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -34,6 +34,18 @@ variable "enable_hub_spoke_peering" {
   default     = false
 }
 
+variable "enable_spoke_routing" {
+  description = "Create spoke route tables and UDRs that point spoke-to-spoke prefixes toward the reserved hub transit IP."
+  type        = bool
+  default     = false
+}
+
+variable "hub_transit_ip" {
+  description = "Reserved future network virtual appliance IP in the hub. Stage 03 uses it to show that UDR intent is not the same as a functioning transit device."
+  type        = string
+  default     = "10.0.4.4"
+}
+
 variable "tags" {
   description = "Additional tags applied to LIFELINE resources."
   type        = map(string)
